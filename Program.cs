e3314863ddfd944b4e7e0ae9a5c4787d555f7345
Brandon P <pew16002@byui.edu>
2024-05-18 22:55:40 -0600

    a

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using System.Collections.Generic;
 
@@ -32,3 +33,39 @@ class Program
         myResume.Display();
     }
 }
+=======
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create two job instances
+        Job job1 = new Job();
+        job1._jobTitle = "IT Specialist";
+        job1._company = "Columbia College";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "IT Specialist";
+        job2._company = "Vivint";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        // Display job details using Display method
+        job1.Display();
+        job2.Display();
+
+        // Create a resume instance and add jobs
+        Resume myResume = new Resume();
+        myResume._personName = "Brandon Pew";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume details using Display method
+        myResume.Display();
+    }
+}
+>>>>>>> 50703103753cead6e77f66e239df86a9f2736518
--- resume.cs
+++ resume.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System; // Assuming the Job class is in the same namespace as Resume class
 // If Job class is in a different namespace, import that namespace here
 
@@ -16,3 +17,23 @@ public class Resume
         }
     }
 }
+=======
+using System; // Assuming the Job class is in the same namespace as Resume class
+// If Job class is in a different namespace, import that namespace here
+
+public class Resume
+{
+    public string _personName = "Brandon Pew"; // Updated with the name "Brandon Pew"
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+>>>>>>> 50703103753cead6e77f66e239df86a9f2736518
